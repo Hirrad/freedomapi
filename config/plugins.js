ea93491473b72e94ab8dd85efb1977052b65b740
Hirrad <hirad@i.ua>
2022-05-18 12:09:23 +0300

    upgrade

--- config/plugins.js
+++ config/plugins.js
@@ -1,13 +1,30 @@
-module.exports = ({ env })=>({
+// module.exports = ({ env })=>({
+//   upload: {
+//     provider: 'ftp-v2',
+//     providerOptions: {
+//       host: env('FTP_HOST'),
+//       port: env('FTP_PORT'),
+//       user: env('FTP_USER'),
+//       password: env('FTP_PASSWORD'),
+//       basePath: env('FTP_BASEPATH'),
+//       baseUrl: env('FTP_BASEURL'),
+//     },
+//   },
+// });
+module.exports = ({ env }) => ({
+  // ...
   upload: {
-    provider: 'ftp-v2',
-    providerOptions: {
-      host: env('FTP_HOST'),
-      port: env('FTP_PORT'),
-      user: env('FTP_USER'),
-      password: env('FTP_PASSWORD'),
-      basePath: env('FTP_BASEPATH'),
-      baseUrl: env('FTP_BASEURL'),
+    config: {
+      provider: 'aws-s3',
+      providerOptions: {
+        accessKeyId: env('AWS_ACCESS_KEY_ID'),
+        secretAccessKey: env('AWS_ACCESS_SECRET'),
+        region: env('AWS_REGION'),
+        params: {
+          Bucket: env('AWS_BUCKET'),
+        },
+      },
     },
   },
+  // ...
 });
