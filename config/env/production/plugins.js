4efa8cbd5e50270d902f9c622985092e1d1dcc1a
Hirrad <hirad@i.ua>
2022-05-30 23:09:46 +0300

    fix

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -1,15 +1,17 @@
-module.exports = ({ env })=>({
+module.exports = ({ env }) => ({
+  // ...
   upload: {
     config: {
-      provider: 'ftp-v2',
+      provider: 'aws-s3',
       providerOptions: {
-        host: env('FTP_HOST'),
-        port: env('FTP_PORT'),
-        user: env('FTP_USER'),
-        password: env('FTP_PASSWORD'),
-        basePath: env('FTP_BASEPATH'),
-        baseUrl: env('FTP_BASEURL'),
+        accessKeyId: env('AWS_ACCESS_KEY_ID'),
+        secretAccessKey: env('AWS_ACCESS_SECRET'),
+        region: env('AWS_REGION'),
+        params: {
+          Bucket: env('AWS_BUCKET'),
+        },
       },
     },
   },
+  // ...
 });
