2b4ca14ea93e40a898a6aea60a3cf653a3637860
Hirrad <hirad@i.ua>
2022-06-15 22:46:41 +0300

    maximum login length 20, added the ability to receive popular * user data on request

--- src/extensions/users-permissions/strapi-server.js
+++ src/extensions/users-permissions/strapi-server.js
@@ -1,33 +1,32 @@
-// module.exports = plugin => {
-//     const sanitizeOutput = (user) => {
-//       const {
-//         password, resetPasswordToken, confirmationToken, ...sanitizedUser
-//       } = user; // be careful, you need to omit other private attributes yourself
-//       return sanitizedUser;
-//     };
+module.exports = plugin => {
+    const sanitizeOutput = (user) => {
+      const {
+        password, resetPasswordToken, confirmationToken, ...sanitizedUser
+      } = user; // be careful, you need to omit other private attributes yourself
+      return sanitizedUser;
+    };
   
-//     plugin.controllers.user.auth = async (ctx) => {
-//       if (!ctx.state.user) {
-//         return ctx.unauthorized();
-//       }
-//       const user = await strapi.entityService.findOne(
-//         'plugin::users-permissions.user',
-//         ctx.state.user.id,
-//         { populate: ['avatar'] }
-        
-//       );
-//       console.log(user);
-//       ctx.body = sanitizeOutput(user);
-//     };
+    plugin.controllers.user.me = async (ctx) => {
+      if (!ctx.state.user) {
+        return ctx.unauthorized();
+      }
+      const user = await strapi.entityService.findOne(
+        'plugin::users-permissions.user',
+        ctx.state.user.id,
+        { populate: ['avatar'] }
+      );
   
-//     plugin.controllers.user.find = async (ctx) => {
-//       const users = await strapi.entityService.findMany(
-//         'plugin::users-permissions.user',
-//         { ...ctx.params, populate: [ 'avatar'] }
-//       );
+      ctx.body = sanitizeOutput(user);
+    };
   
-//       ctx.body = users.map(user => sanitizeOutput(user));
-//     };
+    plugin.controllers.user.find = async (ctx) => {
+      const users = await strapi.entityService.findMany(
+        'plugin::users-permissions.user',
+        { ...ctx.params, populate: ['avatar'] }
+      );
   
-//     return plugin;
-//   };
+      ctx.body = users.map(user => sanitizeOutput(user));
+    };
+  
+    return plugin;
+  };
